2fe070ec19288f2f9be5fdf7a0ef730da721ac79
chuk48 <harouplanet@gmail.com>
2024-09-16 23:23:47 +0200

    Fix routing issue with Netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   build: {
     outDir: "dist",
@@ -15,6 +16,5 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-    base: "/",
   },
 });
