ea1676fb29fe570e3e379f64031c68517d16aaab
chuk48 <harouplanet@gmail.com>
2024-09-20 17:42:58 +0200

    Updated API URLs to Render

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+
 import connectDB from "./config/db.js";
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -10,12 +10,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT || 5000;
 connectDB();
-app.use(
-  cors({
-    origin: "https://big-money-business.netlify.app", // Allow requests from your Netlify domain
-    credentials: true, // If you need to allow credentials like cookies
-  })
-);
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
