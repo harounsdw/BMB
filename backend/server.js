a1a53dfd2cd64bfdabb83ddbf6c2152c5fa06068
chuk48 <harouplanet@gmail.com>
2024-09-21 00:59:12 +0200

    Fix CORS configuration to allow Netlify

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectDB from "./config/db.js";
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -11,7 +12,14 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "https://big-money-business.netlify.app", // Replace with your Netlify domain
+    credentials: true, // Allows cookies and credentials
+    methods: "GET,POST,PUT,DELETE,OPTIONS", // Allow specific HTTP methods
+    allowedHeaders: "Content-Type,Authorization", // Allow specific headers
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
