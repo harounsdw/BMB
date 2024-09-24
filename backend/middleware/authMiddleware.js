2455b2d916b591291f0023536e384cbd67edf50e
chuk48 <harouplanet@gmail.com>
2024-09-25 00:06:15 +0200

    back stuff

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,28 +1,36 @@
 import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../Models/userModel.js";
 
-const protect = (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
   let token;
 
-  // Check for token in cookies
-  if (req.cookies && req.cookies.jwt) {
-    try {
-      token = req.cookies.jwt; // Get the token from the cookie
+  // Check for token in Authorization header or in cookies
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
 
-      // Verify token
+  if (token) {
+    try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Attach user info to request object
-      req.user = decoded.userId;
+      req.user = await User.findById(decoded.userId).select("-password");
 
-      next(); // Proceed to the next middleware
+      next();
     } catch (error) {
+      console.error(error);
       res.status(401);
       throw new Error("Not authorized, token failed");
     }
   } else {
     res.status(401);
-    throw new Error("Not authorized, token missing");
+    throw new Error("Not authorized, no token");
   }
-};
+});
 
 export { protect };
