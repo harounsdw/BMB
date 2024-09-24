2d4a716d5793963cb8affc6abf1216a07cc014c6
chuk48 <harouplanet@gmail.com>
2024-09-24 12:57:46 +0200

    edit stuff

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -18,7 +18,7 @@ router.post("/logout", logoutUser);
 router
   .route("/profile")
   .get(protect, getUserProfile)
-  .put(protect, updateUserProfile);
+  .patch(protect, updateUserProfile);
 
 router.put("/transfer-points", transferPoints);
 
--- frontend/src/components/admin/admin.jsx
+++ frontend/src/components/admin/admin.jsx
@@ -102,7 +102,7 @@ const Admin = () => {
         };
 
         // Make the API request using Axios
-        const { data } = await axios.put(
+        const { data } = await axios.patch(
           `https://bmb-9bgg.onrender.com/api/users/profile`,
           {
             _id: userInfo._id,
