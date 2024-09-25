abfa3b530126ac357d87ee9674907cc2cab25012
chuk48 <harouplanet@gmail.com>
2024-09-25 23:10:50 +0200

    back stuff

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,21 +1,26 @@
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import User from "../Models/userModel.js";
-
 const protect = asyncHandler(async (req, res, next) => {
   let token;
 
-  token = req.cookies.jwt;
+  if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  } else if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
 
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
       req.user = await User.findById(decoded.userId).select("-password");
-
+      console.log("Authenticated user:", req.user); // Log to check if user is set
       next();
     } catch (error) {
-      console.error(error);
+      console.error("Token failed:", error);
       res.status(401);
       throw new Error("Not authorized, token failed");
     }
