5a4dc8dda6fa69aa901c1ed8b01502b950fe1090
chuk48 <harouplanet@gmail.com>
2024-09-26 23:25:37 +0200

    back stuff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: "/",
   build: {
+    outDir: "fontend/dist",
     rollupOptions: {
       external: ["./react-bootstrap", "./react-icons/fa"],
     },
