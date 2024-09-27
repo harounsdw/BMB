3da2cad174f0ac1cf582a1d28a7b589039c34ebf
chuk48 <harouplanet@gmail.com>
2024-09-27 15:55:30 +0200

    token stuff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: "/",
   build: {
-    outDir: "frontend/dist",
+    outDir: "dist",
     rollupOptions: {
       external: ["./react-bootstrap", "./react-icons/fa"],
     },
