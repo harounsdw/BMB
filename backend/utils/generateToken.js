87a08d81fd2f0a4fcb51f09e51054cb7fa53e00d
chuk48 <harouplanet@gmail.com>
2024-09-30 22:53:25 +0200

    token two stuff

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "production", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+    secure: process.env.NODE_ENV === "production", // Use 'secure' flag for production
+    sameSite: "None", // Important for cross-site cookies
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
