2418f552d3ecb04cd83b5bb2e4fae51e546a398b
chuk48 <harouplanet@gmail.com>
2024-10-09 23:49:41 +0200

    token fsr stuff

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,10 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+// Include credentials so cookies (JWT token) are sent with requests
+const baseQuery = fetchBaseQuery({
+  baseUrl: "",
+  credentials: "include", // This ensures cookies are sent with the requests
+});
 
 export const apiSlice = createApi({
   baseQuery,
