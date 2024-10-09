67f74c746dc7825a1630bdb616e1fa4b4ffb909d
chuk48 <harouplanet@gmail.com>
2024-10-09 23:52:56 +0200

    token fskr stuff

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,13 +2,14 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: "30d", // Token expiration
   });
 
+  // Set the token as an HTTP-only cookie
   res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+    httpOnly: true, // Cookie can't be accessed via client-side scripts
+    secure: process.env.NODE_ENV === "production", // Only use HTTPS in production
+    sameSite: "Strict", // Ensures cookie is sent only to same-site requests
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
