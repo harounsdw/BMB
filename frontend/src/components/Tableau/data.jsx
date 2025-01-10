717ac1f376525b1d8be8068c447591a16b8ac033
chuk48 <harouplanet@gmail.com>
2025-01-10 18:08:47 +0100

    toke sdzb stuff

--- frontend/src/components/Tableau/data.jsx
+++ frontend/src/components/Tableau/data.jsx
@@ -2,38 +2,38 @@ const items = [
   {
     id: 1,
     name: " الجيل الأول ",
-    percent: "20%",
+    percent: "33%",
     wallet: "30",
   },
 
   {
     id: 2,
     name: " الجيل الثاني  ",
-    percent: "15%",
+    percent: "25%",
     wallet: "22",
   },
   {
     id: 3,
     name: " الجيل الثالث ",
-    percent: "10%",
+    percent: "20%",
     wallet: "15",
   },
   {
     id: 4,
     name: " الجيل الرابع ",
-    percent: "8%",
+    percent: "15%",
     wallet: "12",
   },
   {
     id: 5,
     name: " الجيل الخامس ",
-    percent: "6%",
+    percent: "10%",
     wallet: "9",
   },
   {
     id: 6,
     name: " الجيل السادس ",
-    percent: "3%",
+    percent: "10%",
     wallet: "5",
   },
 ];
