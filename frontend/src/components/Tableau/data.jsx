526f85e4f58ed3c44460f7a4353ccf300ca47eaa
chuk48 <harouplanet@gmail.com>
2025-01-10 18:24:46 +0100

    toke sdb stuff

--- frontend/src/components/Tableau/data.jsx
+++ frontend/src/components/Tableau/data.jsx
@@ -3,38 +3,38 @@ const items = [
     id: 1,
     name: " الجيل الأول ",
     percent: "33%",
-    wallet: "30",
+    wallet: "150",
   },
 
   {
     id: 2,
     name: " الجيل الثاني  ",
     percent: "25%",
-    wallet: "22",
+    wallet: "150",
   },
   {
     id: 3,
     name: " الجيل الثالث ",
     percent: "20%",
-    wallet: "15",
+    wallet: "150",
   },
   {
     id: 4,
     name: " الجيل الرابع ",
     percent: "15%",
-    wallet: "12",
+    wallet: "150",
   },
   {
     id: 5,
     name: " الجيل الخامس ",
     percent: "10%",
-    wallet: "9",
+    wallet: "150",
   },
   {
     id: 6,
     name: " الجيل السادس ",
     percent: "10%",
-    wallet: "5",
+    wallet: "150",
   },
 ];
 export default items;
